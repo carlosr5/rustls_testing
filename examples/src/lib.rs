9bf3f2621d72ff7f4d84b820c927512ab5ef4052
Carlos Ramirez <t-caramirez@microsoft.com>
2023-06-21 13:35:58 -0700

    Adding basic Criterion benchmark testing

---
+++ examples/src/lib.rs
@@ -0,0 +1,17 @@
+#[inline]
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
